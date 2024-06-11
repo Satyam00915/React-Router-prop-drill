33d61647c5d9c284af3e6f8646a4124d6981f3fe
Satyam00915 <satyamupadhyay00915@gmail.com>
2024-06-11 16:00:35 +0530

    Files Rename

--- context-api/src/App.jsx
+++ context-api/src/App.jsx
@@ -1,48 +1,64 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { CountContext } from "./components/context";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  //wrap anyone that wants to use the teleported value inside the provider
+
   return (
     <>
       <div>
-        <Count />
-        <Button />
+        <CountContext.Provider value={{ count, setCount }}>
+          <Count />
+        </CountContext.Provider>
       </div>
     </>
   );
 }
 
-function Count({ count }) {
+function Count() {
+  const { count } = useContext(CountContext);
   return (
     <>
       <div>{count}</div>
+      <Button />
     </>
   );
 }
 
 function Button() {
+  const { count, setCount } = useContext(CountContext);
+
   return (
     <>
       <div>
         <button
           onClick={() => {
-            setCount;
+            setCount(count + 1);
           }}
         >
           Increase
         </button>
         <button
           onClick={() => {
-            setCount;
+            setCount(count - 1);
           }}
         >
           Decrease
         </button>
       </div>
+    </>
+  );
+}
+
+function Countrenderer() {
+  return (
+    <>
+      <div></div>
     </>
   );
 }
